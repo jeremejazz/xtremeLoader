76feb8910ff794790fd953efeb799c6867924611
jeremejazz <jeremejazz15@yahoo.com>
2016-12-21 16:11:22 +0800

    automation for building page

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,11 @@ module.exports = function(grunt) {
 
 
  
-
+  var target = grunt.option('target');
+  var extension = "";
+  if (target  ==  'minify'){
+    extension = ".min.";
+  }
 
 	  grunt.initConfig({
 	    pkg: grunt.file.readJSON('package.json')
@@ -39,21 +43,68 @@ module.exports = function(grunt) {
  	},
  	js: {
  		src: ['src/js/jquery.js', 'src/js/jquery.mobile-1.4.5.js' ],
- 		dest: 'app.js'
+ 		dest: 'build/<%= pkg.name %>.js'
  	},
  	css: {
-
+    src: ['src/css/font-awesome.min.css' , 'src/css/jquery.mobile-1.4.5.css', 'src/css/jquerymobile.nativedroid.css' ],
+    dest: 'build/<%= pkg.name %>.css'
  	}
 
  });
 
+ 
+grunt.loadNpmTasks('grunt-html-build');
+grunt.config('htmlbuild', {
+	dist:{
+		src: 'index.html',
+		dest: 'build',
+		options:{
+      scripts:{
+        bundle: ['build/<%= pkg.name %>' + extension +'.js']
+      },
+      styles: {
+        bundle: ['build/<%= pkg.name %>' + extension + '.css']
+      },
+			sections: {
+				layout: {
+					navbar: 'sections/navbar.html'
+				}
+			}
+		}
+	}
+
+});
 
 
-
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.config('copy', {
+    main:{
+      files:[
+        {
+          expand: true, 
+          src: [
+            'src/css/fonts/**',
+            'src/css/images/**',
+            'src/css/jquerymobile.nativedroid.*.css'
+            ],
+          dest: 'build/'
+        }
+      ]
+    }
+  });
 
 
   // Default task(s).
-  var defaultTasks = [ 'uglify', 'cssmin'];
+
+
+var defaultTasks = ['copy'];
+  if (target == "minify"){
+      defaultTasks.concat([ 'uglify', 'cssmin', 'htmlbuild']);
+  }else{
+      defaultTasks.concat( [ 'concat', 'htmlbuild'] );
+  }
+  
+
  
   grunt.registerTask('default', defaultTasks);
   grunt.registerTask('compile', ['concat']);
